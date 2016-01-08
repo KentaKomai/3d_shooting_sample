1513ff9ebfbe1ee074770739452d053542affc93
kkomai <komaikenta@team-lab.com>
2016-01-08 21:43:05 +0900

    add prefab

---
+++ Assets/Debri.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Debri : MonoBehaviour {
+
+    public float angle = 30F;
+    public int score = 10;
+    public Vector3 targetPos;
+
+	// Use this for initialization
+	void Start () {
+        Transform target = GameObject.Find("Earth").transform;
+        targetPos = target.position;
+        this.transform.LookAt(target);
+        this.transform.Rotate(new Vector3(0, 0, Random.Range(0, 30)), Space.World);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 axis = this.transform.TransformDirection(Vector3.up);
+        this.transform.RotateAround(targetPos, axis, angle * Time.deltaTime);
+	}
+
+    void OnMouseDown() {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/SpawnPoint.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnPoint : MonoBehaviour {
+
+    public GameObject debri;
+    public float interval = 1F;
+
+    // Use this for initialization
+    void Start() {
+        StartCoroutine("SpawnDebris");
+    }
+
+    IEnumerator SpawnDebris() {
+        while(true) {
+            Instantiate(debri, transform.position, Quaternion.identity);
+            yield return new WaitForSeconds(interval);
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
